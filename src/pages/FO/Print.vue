043df19d238dc6490f5a6c8c8652ff2f6dd5f3f3
Aura Putri K <aura232kireina@gmail.com>
2024-01-09 10:22:32 +0700

    Slicing UI Print

--- src/pages/FO/Print.vue
+++ src/pages/FO/Print.vue
@@ -11,9 +11,43 @@
       </template>
     </FOMenubar>
 
-    <div class="row justify-between content-between q-ma-md">
-      <iframe src="../../assets/pdf/invoicePdf.pdf" frameborder="0" style="min-width: 50%;"></iframe>
-      <div class="column justify-between" style="min-width: 50%; gap: 15px;">
+    <div class="row q-ma-md no-wrap" style="gap: 15px" >
+      <div style="width: 50%;">
+        <q-img
+          src="../../assets/img/lingian-logo-colored.png"
+          style="width: 10rem; height: 10rem; display: flex; justify-content: center; align-items: center;"
+          class="q-mx-auto"
+        />
+        <div class="my-table">
+          <q-table
+            class="no-shadow scrollable-table q-mt-md"
+            :rows="data"
+            :columns="columns"
+          >
+            <template>
+              <q-tr class="table-head">
+                <q-th style="padding-top: 0px; padding-bottom: 0px">
+                  <template v-slot:header="props">
+                    <q-tr class="table-head" :props="props">
+                      <q-th
+                        v-for="(col, i) in props.cols"
+                        :key="i"
+                        style="padding-top: 0px; padding-bottom: 0px"
+                      >
+                      </q-th>
+                    </q-tr>
+                  </template>
+                  <template v-slot:body="props">
+                    <q-tr :props="props"> </q-tr>
+                  </template>
+                </q-th>
+              </q-tr>
+            </template>
+          </q-table>
+        </div>
+      </div>
+      <!-- <iframe src="src/assets/pdf/invoicePdf.pdf" frameborder="0"></iframe> -->
+      <div class="column justify-between" style="width: 50%; gap: 15px;">
         <div class="column" style="gap: 10px;">
           <div class="row justify-between" style="overflow: auto; min-width: 100%; max-width: 100%;">
             <p class="q-my-auto" style="min-width: 60%; max-width: 60%;">Page</p>
@@ -108,6 +142,19 @@
 import { defineComponent, ref } from 'vue'
 import FOMenubar from 'src/components/FOMenubar.vue'
 
+const columns = [
+  { name: 'Art', label: 'Art', align: 'left', field: 'Art' },
+  { name: 'Qty', label: 'Qty', align: 'left', field: 'Qty' },
+  { name: 'Description', label: 'Description', align: 'left', field: 'Description' },
+  { name: 'Rate', label: 'Rate', align: 'left', field: 'Rate' },
+  { name: 'Amount', label: 'Amount', align: 'left', field: 'Amount' },
+  { name: 'RmNo', label: 'RmNo', align: 'left', field: 'RmNo' },
+  { name: 'RoomBoy', label: 'Room Boy', field: 'RoomBoy', align: 'left' },
+  { name: 'VoucherNumber', label: 'Voucher Number', align: 'left', field: 'VoucherNumber' },
+  { name: 'BillDate', label: 'BillDate', align: 'left', field: 'BillDate' },
+  { name: '', label: '', align: 'center', field: '' }
+]
+
 export default defineComponent({
   name: 'Print',
   setup() {
@@ -122,8 +169,20 @@ export default defineComponent({
       pagesperOpt: ['1', 'Custom'],
       margin: ref(null),
       marginOpt: ['Default', 'Custom'],
+      columns,
     }
   },
   components: { FOMenubar }
 })
 </script>
+
+<style>
+.half-width-container {
+  max-width: 50%; /* Sesuaikan dengan persentase yang diinginkan */
+  margin: auto; /* Agar tetap di tengah */
+}
+.scrollable-table {
+  max-height: 100%;
+  overflow-y: auto;
+}
+</style>

dccae263021f67f102ebefb86a13e4fb317f7f12
Razan Muhammad <razanmfs507@gmail.com>
2024-01-29 10:12:45 +0700

    fix guest list incognito

--- src/pages/FO/GuestList.vue
+++ src/pages/FO/GuestList.vue
@@ -55,8 +55,8 @@
       </template>
       <template #right>
         <q-separator vertical />
-        <q-btn flat square color="primary" icon="pending_actions" @click="showhistory(!this.state)">
-          <q-tooltip>History</q-tooltip>
+        <q-btn flat square color="primary" icon="pending_actions">
+          <q-tooltip>Pending</q-tooltip>
         </q-btn>
       </template>
     </FOMenubar>
@@ -120,7 +120,7 @@
               <q-tr :props="props">
                 <template v-for="(cell, key, i) in props.row" :key="i">
                   <q-td
-                    v-if="!['ResRoomNo'].includes(key) && !['Roomno'].includes(key)"
+                    v-if="!['ResRoomNo'].includes(key)"
                     :style="
                       cell.style.bordercolor
                         ? 'color:' +
@@ -135,19 +135,14 @@
                     "
                   >
                     {{ cell.data }}
-                    <q-popup-edit
-                      v-model="props.row.name"
-                      title=""
-                      auto-save
-                      style="width: fit-content; padding: 0; margin: 0"
-                    >
-                      <q-list style="align-content: flex; width: 100%; padding: 5px">
+                    <q-popup-edit v-model="props.row.name" title="" auto-save>
+                      <q-list style="align-content: flex-end; width: 100%">
                         <q-item
                           clickable
                           v-close-popup
                           @click="changeinc(props.row)"
-                          style="display: flex; background-color: red"
-                          class="q-px-xs"
+                          class="q-px-xl"
+                          style="display: flex; background-color: red; border-radius: 30px"
                         >
                           <q-item-section>
                             <q-item-label style="color: black" class="font-bold"
@@ -172,15 +167,13 @@
                           style="display: flex"
                         >
                           <q-item-section>
-                            <q-item-label class="font-bold" @click="waitinglist(props.row, false)"
-                              >Waiting List</q-item-label
-                            >
+                            <q-item-label class="font-bold">Waiting List</q-item-label>
                           </q-item-section>
                         </q-item>
                         <q-item
                           clickable
                           v-close-popup
-                          @click="addnewextrabed(props.row)"
+                          @click="setRoomResv(props.row)"
                           style="display: flex"
                         >
                           <q-item-section>
@@ -199,38 +192,6 @@
                         </q-item>
                       </q-list>
                     </q-popup-edit>
-                    <q-dialog v-model="dialog2">
-                      <q-card>
-                        <q-card-section>
-                          <div class="text-h6">Waiting List</div>
-                          <q-separator horizontal class="q-ma-xs" />
-                          <div class="q-mt-sm">
-                            Do you want to request something for room number {{ this.roomno
-                            }}<br />Please Give Us Your Request
-                          </div>
-                          <q-input
-                            v-model="waitingnote"
-                            class="q-my-md"
-                            label="Note..."
-                            dense
-                            outlined
-                            type="textarea"
-                          />
-                          <div
-                            class="row items-center"
-                            style="width: 100%; justify-content: space-between"
-                          >
-                            <q-btn v-close-popup label="Cancle" outline color="primary" />
-                            <q-btn
-                              v-close-popup
-                              label="accept"
-                              @click="setRoomResv(props.row)"
-                              color="primary"
-                            />
-                          </div>
-                        </q-card-section>
-                      </q-card>
-                    </q-dialog>
                   </q-td>
                 </template>
                 <q-td key="" :props="props" style="width: 10px">
@@ -247,7 +208,7 @@
                         test
                       </div> -->
                     </div>
-                    <div>
+                    <div class="q-px-md">
                       <q-btn auto-close flat round color="primary" icon="more_vert">
                         <q-menu>
                           <q-list style="align-content: flex-end; width: 100%">
@@ -261,7 +222,7 @@
                                 flat
                                 rounded
                                 size="13px"
-                                @click="editroom(props.row)"
+                                @click="setRoomResv(props.row)"
                                 style="color: #008444"
                                 ><svg
                                   width="19"
@@ -343,43 +304,41 @@
                                 icon="edit_note"
                               />
                               <q-item-section>
-                                <q-item-label>Change Room</q-item-label>
+                                <q-item-label>Edit Room</q-item-label>
                               </q-item-section>
                             </q-item>
+
+                            <q-dialog v-model="dialogeditroom" persistent>
+                              <q-card>
+                                <q-card-section class="row items-center">
+                                  <q-avatar
+                                    icon="signal_wifi_off"
+                                    color="primary"
+                                    text-color="white"
+                                  />
+                                  <span class="q-ml-sm"
+                                    >You are currently not connected to any network.</span
+                                  >
+                                </q-card-section>
+
+                                <q-card-section class="row items-center">
+                                  <q-toggle v-model="cancelEnabled" label="Cancel button enabled" />
+                                </q-card-section>
+
+                                <!-- Notice v-close-popup -->
+                                <q-card-actions align="right">
+                                  <q-btn
+                                    flat
+                                    label="Cancel"
+                                    color="primary"
+                                    v-close-popup="cancelEnabled"
+                                  />
+                                  <q-btn flat label="Turn on Wifi" color="primary" v-close-popup />
+                                </q-card-actions>
+                              </q-card>
+                            </q-dialog>
                           </q-list>
                         </q-menu>
-                        <q-dialog v-model="dialogeditroom" ref="editRoomDialog">
-                          <q-card>
-                            <q-card-section>
-                              <div class="text-h6">Change Room</div>
-                              <q-separator horizontal class="q-ma-xs" />
-                              <div class="q-mt-sm">
-                                Do you want to request something for room number {{ this.roomno
-                                }}<br />Please Give Us Your Request
-                              </div>
-                              <q-input
-                                v-model="waitingnote"
-                                class="q-my-md"
-                                label="Note..."
-                                dense
-                                outlined
-                                type="textarea"
-                              />
-                              <div
-                                class="row items-center"
-                                style="width: 100%; justify-content: space-between"
-                              >
-                                <q-btn v-close-popup label="Cancle" outline color="primary" />
-                                <q-btn
-                                  v-close-popup
-                                  label="accept"
-                                  @click="waitinglist(props.row, true)"
-                                  color="primary"
-                                />
-                              </div>
-                            </q-card-section>
-                          </q-card>
-                        </q-dialog>
                       </q-btn>
                     </div>
                   </div>
@@ -409,13 +368,9 @@ export default defineComponent({
   components: { FOMenubar, MultiPane, GuestForm },
   setup() {
     return {
-      state: ref(false),
-      waitingnote: ref(''),
-      dialog2: ref(false),
       dialogeditroom: ref(false),
       cancelEnabled: ref(true),
       iconName1: 'more_vert',
-      roomno: ref(),
       showDropdown: false,
       background: ref(),
       allObjectsInArray,
@@ -654,70 +609,6 @@ export default defineComponent({
     }
   },
   methods: {
-    addnewextrabed(data) {
-      const resvId = data['ResNo'].data
-      const roomNo = data['ResRoomNo'].data
-      // const extra = {
-      //   articleId: 110,
-      //   qty: 1
-      // }
-      // const extra2 = {
-      //   articleId: 0,
-      //   qty: 1
-      // }
-      const articledata = [
-        {
-          articleId: 110,
-          qty: 1
-        }
-      ]
-      try {
-        this.api.post(
-          `/invoice/${resvId}/${roomNo}/article`,
-          articledata,
-          ({ status, data, message }) => {
-            if (status === 200) {
-              this.triggerPositive(message)
-              console.log(data)
-            }
-          }
-        )
-      } catch (error) {
-        console.error('error : ' + error)
-      }
-    },
-    showhistory(state) {
-      console.log(state)
-      this.state = state
-      this.fetchData()
-    },
-    waitinglist(data, log) {
-      const roomNo = data['RmNo'].data
-      console.log(roomNo)
-      this.roomno = roomNo
-      // this.dialogeditroom = true
-      // console.log(this.dialogeditroom)
-      this.$ResvStore.fix = false
-      this.$ResvStore.ds = false
-      this.$ResvStore.logc = true
-
-      if (log === true && this.waitingnote != null && this.waitingnote != '') {
-        // this.$refs.editRoomDialog.hide();
-        console.log(this.waitingnote)
-        this.$ResvStore.waitingnote = this.waitingnote
-        this.$ResvStore.currentResvId = data['ResNo'].data
-        this.$ResvStore.currentRoomResvId = data['ResRoomNo'].data
-      } else {
-        this.triggerNegative('note has not been filled in, data must be filled in')
-      }
-    },
-    editroom(data) {
-      this.$ResvStore.currentResvId = data['ResNo'].data
-      this.$ResvStore.currentRoomResvId = data['ResRoomNo'].data
-      this.$ResvStore.fix = false
-      this.$ResvStore.ds = true
-      this.$ResvStore.logc = false
-    },
     changereset(data) {
       try {
         const resvId = data['ResNo'].data
@@ -801,14 +692,13 @@ export default defineComponent({
     setRoomResv(data) {
       this.$ResvStore.currentResvId = data['ResNo'].data
       this.$ResvStore.currentRoomResvId = data['ResRoomNo'].data
+      this.$ResvStore.fix = false
     },
     fixDetail(data) {
       this.$ResvStore.currentResvId = data['ResNo'].data
       this.$ResvStore.currentRoomResvId = data['ResRoomNo'].data
       this.$ResvStore.fix = true
       console.log(this.$ResvStore.fix)
-      this.$ResvStore.ds = false
-      this.$ResvStore.logc = false
     },
     async deleteResv(data) {
       // const resvId = data['ResNo']?.data
@@ -818,8 +708,8 @@ export default defineComponent({
         const roomNo = data['ResRoomNo'].data
         console.log(roomNo)
         this.api.delete(`detail/reservation/${resvId}/${roomNo}/delete`, ({ data }) => {
+          // window.location.reload()
           this.triggerPositive(data.message)
-          window.location.reload()
         })
       } catch (error) {
         console.error('Terjadi kesalahan, mohon coba lagi')
@@ -857,13 +747,10 @@ export default defineComponent({
     fetchData() {
       this.loading = true
 
-      let url = `arrival?page=${this.pagination.page}&perPage=${this.pagination.rowsPerPage}&history=${this.state}`
+      let url = `arrival?page=${this.pagination.page}&perPage=${this.pagination.rowsPerPage}`
 
       if (this.filterDisplay !== null) url += `&disOpt=${this.filterDisplay}`
 
-      // if(this.state === true) url += `&history=${this.state}`
-
-      console.log(url)
       if (this.filterSortOrder['col'] != '' && this.filterSortOrder['val'] != '')
         url += `&sortOrder=${this.filterSortOrder['val']}`
 
@@ -895,16 +782,6 @@ export default defineComponent({
     getUniqueRoomBoys(roomBoys) {
       return roomBoys.map((boy) => boy.name)
     },
-    triggerNegative(data) {
-      this.$q.notify(
-        {
-          type: 'negative',
-          message: data || 'error',
-          timeout: 1000
-        },
-        4000
-      )
-    },
     triggerPositive(message) {
       this.$q.notify(
         {
@@ -920,18 +797,16 @@ export default defineComponent({
 
       raw.forEach((rsrv) => {
         rsrv.reservation.forEach((rr) => {
-          const [backgroundColor] =
+          const [backgroundColor, color] = [
             rr.reservation.resvStatus.rowColor === '#f10000'
               ? rr.reservation.resvStatus.rowColor
-              : '#ffffff'
-          const color =
-            rr.reservation.resvStatus.textColor === '#f10000' ||
-            rr.reservation.resvStatus.textColor === '#808080'
+              : '#ffffff',
+            rr.reservation.resvStatus.textColor === '#f10000'
               ? rr.reservation.resvStatus.textColor
               : '#000000'
+            // color: rr.reservation.resvStatus.textColor
+          ]
           const { id } = rr.arrangment
-          console.log(rr.room.id)
-          this.roomno = rr.room.id
           list.push({
             ResNo: { data: rsrv.reservationId, style: { backgroundColor, color } },
             ResRoomNo: { data: rr.id, style: { backgroundColor } },
@@ -969,12 +844,8 @@ export default defineComponent({
             CreatedDate: {
               data: formatDate(rr.reservation.created_at),
               style: { backgroundColor, color }
-            },
-            Roomno: {
-              data: rr.room.id
             }
           })
-          console.log(list)
         })
       })
       this.data = list

350a2b0073238823ecd548266f85b05ee8b100d8
Harmoni Natanael S <harmoninatanaels@gmail.com>
2024-02-20 09:34:21 +0700

    Fix Refresh Token issue

--- src/services/api/refresh_token.js
+++ src/services/api/refresh_token.js
@@ -5,14 +5,13 @@ import { Config } from '../config'
 export const refreshToken = async () => {
   try{
     const api = new Api()
-    console.log('API GET')
     await api.get('/auth/user/refresh', ({ status, data }) => {
-      console.log(status, data)
       if (status == 200 && data['accessToken']) {
         authStore().setAccessToken(data['accessToken'])
         return data['accessToken']
       }else {
         Config.logout()
+        window.location.reload()
         return null
       }
     })
